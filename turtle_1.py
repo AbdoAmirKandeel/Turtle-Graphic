ea668306b1e7dae92fc1ac2c5c6f2e84eed2ac13
AbdoAmirKandeel <159439735+AbdoAmirKandeel@users.noreply.github.com>
2024-02-08 14:55:43 +0300

    turtle_1.py

--- turtle_1.py
+++ turtle_1.py
@@ -1,10 +1,17 @@
+#imports
 import colorsys
 import turtle
+
+#Windows
 t = turtle.Turtle()
 s = turtle.Screen().bgcolor('black')
+
+#Speed
 t.speed(1000)
 n = 90
 h = 10
+
+#Circle
 for i in range (360):
     c = colorsys.hsv_to_rgb(h, 1, 0.8)
     h += 1/n
@@ -14,3 +21,5 @@ for i in range (360):
     for j in range(2):
         t.left(2)
         t.circle(100)
+
+#End
